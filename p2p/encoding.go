ec9f8148a49948fe2aea4b6fc2c57f5624ab456d
daniilkuz <d.d.kuzovlev@gmail.com>
2024-09-08 15:04:48 +0300

    added NOPDecoder

--- p2p/encoding.go
+++ p2p/encoding.go
@@ -2,6 +2,7 @@ package p2p
 
 import (
 	"encoding/gob"
+	"fmt"
 	"io"
 )
 
@@ -13,4 +14,18 @@ type GOBDecoder struct{}
 
 func (dec GOBDecoder) Decode(r io.Reader, v any) error {
 	return gob.NewDecoder(r).Decode(v)
+}
+
+type NOPDecoder struct{}
+
+func (dec NOPDecoder) Decode(r io.Reader, msg Message) error {
+	buf := make([]byte, 1024)
+	n, err := r.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(buf[:n]))
+
+	return nil
 }
