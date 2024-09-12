366325c719e7f945899d96a77e368dc92f612193
daniilkuz <d.d.kuzovlev@gmail.com>
2024-09-12 07:07:24 +0300

    fixed test, added opts

--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -7,13 +7,18 @@ import (
 )
 
 func TestTCPTransport(t *testing.T) {
-	listenAddr := ":4000"
-	tr := NewTCPTransport(listenAddr)
 
-	assert.Equal(t, tr.listenAddr, listenAddr)
+	opts := TCPTransportOpts{
+		ListenAddr:    ":3000",
+		HandshakeFunc: NOPHandshakeFunc,
+		Decoder:       DefaultDecoder{},
+	}
+	tr := NewTCPTransport(opts)
+
+	assert.Equal(t, tr.ListenAddr, ":3000")
 
 	assert.Nil(t, tr.ListenAndAccept())
 
-	select {}
+	// select {}
 
 }
