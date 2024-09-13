abc8b24c691da5293d7c034478b234331532fa67
daniilkuz <d.d.kuzovlev@gmail.com>
2024-09-13 07:36:42 +0300

    added rpc channel and Conusme function

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -23,6 +23,10 @@ func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
 	}
 }
 
+func (p *TCPPeer) Close() error {
+	return p.conn.Close()
+}
+
 type TCPTransportOpts struct {
 	ListenAddr    string
 	HandshakeFunc HandshakeFunc
@@ -32,6 +36,7 @@ type TCPTransportOpts struct {
 type TCPTransport struct {
 	TCPTransportOpts
 	listener net.Listener
+	rpcch    chan RPC
 
 	mu    sync.RWMutex
 	peers map[net.Addr]Peer
@@ -40,7 +45,12 @@ type TCPTransport struct {
 func NewTCPTransport(opts TCPTransportOpts) *TCPTransport {
 	return &TCPTransport{
 		TCPTransportOpts: opts,
+		rpcch:            make(chan RPC),
 	}
+}
+
+func (t *TCPTransport) Consume() <-chan RPC {
+	return t.rpcch
 }
 
 func (t *TCPTransport) ListenAndAccept() error {
@@ -77,16 +87,17 @@ func (t *TCPTransport) handleConn(conn net.Conn) {
 		return
 	}
 
-	rpc := &RPC{}
+	rpc := RPC{}
 	for {
-		if err := t.Decoder.Decode(conn, rpc); err != nil {
+		if err := t.Decoder.Decode(conn, &rpc); err != nil {
 			fmt.Printf("TCP error %s\n", err)
 			continue
 		}
 
 		rpc.From = conn.RemoteAddr()
 
-		fmt.Printf("message: %+v\n", rpc)
+		t.rpcch <- rpc
+		// fmt.Printf("message: %+v\n", rpc)
 	}
 
 }
