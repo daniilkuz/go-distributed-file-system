0249b93cb2d3768b79903052230f16130d09ce71
daniilkuz <d.d.kuzovlev@gmail.com>
2024-10-13 14:04:49 +0300

    added logic to main

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/daniilkuz/go-distributed-file-system/p2p"
+)
+
+func main() {
+	tcpTransportOpts := p2p.TCPTransportOpts{
+		ListenAddr:    "3000",
+		HandshakeFunc: p2p.NOPHandshakeFunc,
+		Decoder:       p2p.DefaultDecoder{},
+	}
+	tcpTransport := p2p.NewTCPTransport(tcpTransportOpts)
+	fileServerOpts := FileServerOpts{
+		ListenAddr:        "3000",
+		StoreageRoot:      "3000_netweork",
+		PathTransformFunc: CASPathTransformFunc,
+		Transport:         *tcpTransport,
+	}
+	s := NewFileServer(fileServerOpts)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	select {}
 }
