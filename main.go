3ccb16c3eabf36909725c87ec8c2e4d63bad6215
daniilkuz <d.d.kuzovlev@gmail.com>
2024-10-21 08:13:29 +0300

    added makeServer function

--- main.go
+++ main.go
@@ -2,34 +2,59 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/daniilkuz/go-distributed-file-system/p2p"
 )
 
-func main() {
+func makeServer(listenAddr string, nodes ...string) *FileServer {
 	tcpTransportOpts := p2p.TCPTransportOpts{
-		ListenAddr:    ":3000",
+		ListenAddr:    listenAddr,
 		HandshakeFunc: p2p.NOPHandshakeFunc,
 		Decoder:       p2p.DefaultDecoder{},
 	}
 	tcpTransport := p2p.NewTCPTransport(tcpTransportOpts)
 	fileServerOpts := FileServerOpts{
-		StoreageRoot:      "3000_netweork",
+		StoreageRoot:      listenAddr + "_netweork",
 		PathTransformFunc: CASPathTransformFunc,
 		Transport:         tcpTransport,
-		BootstrapNodes:    []string{":4000"},
+		BootstrapNodes:    nodes,
 	}
-	s := NewFileServer(fileServerOpts)
+	return NewFileServer(fileServerOpts)
+
+}
+
+func main() {
+	// tcpTransportOpts := p2p.TCPTransportOpts{
+	// 	ListenAddr:    ":3000",
+	// 	HandshakeFunc: p2p.NOPHandshakeFunc,
+	// 	Decoder:       p2p.DefaultDecoder{},
+	// }
+	// tcpTransport := p2p.NewTCPTransport(tcpTransportOpts)
+	// fileServerOpts := FileServerOpts{
+	// 	StoreageRoot:      "3000_netweork",
+	// 	PathTransformFunc: CASPathTransformFunc,
+	// 	Transport:         tcpTransport,
+	// 	BootstrapNodes:    []string{":4000"},
+	// }
+	// s := NewFileServer(fileServerOpts)
+
+	// go func() {
+	// 	time.Sleep(time.Second)
+	// 	s.Stop()
+	// }()
+
+	// if err := s.Start(); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	s1 := makeServer(":3000", "")
+	s2 := makeServer(":4000", ":3000")
 
 	go func() {
-		time.Sleep(time.Second)
-		s.Stop()
+		log.Fatal(s1.Start())
 	}()
 
-	if err := s.Start(); err != nil {
-		log.Fatal(err)
-	}
+	s2.Start()
 
 	// select {}
 }
