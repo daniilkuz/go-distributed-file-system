4b0b2879e7e3b94901c97be28c4d552f9972f0ff
daniilkuz <d.d.kuzovlev@gmail.com>
2024-10-24 10:42:34 +0300

    added Send method

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -24,6 +24,11 @@ func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
 	}
 }
 
+func (p *TCPPeer) Send(b []byte) error {
+	_, err := p.conn.Write(b)
+	return err
+}
+
 func (p *TCPPeer) RemoteAddr() net.Addr {
 	return p.conn.RemoteAddr()
 }
