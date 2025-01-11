a136a2152a3d0de29a6cea9eb951ed1b2ead626a
daniilkuz <d.d.kuzovlev@gmail.com>
2025-01-11 15:52:56 +0300

    added Readcloser

--- server.go
+++ server.go
@@ -287,6 +287,12 @@ func (s *FileServer) handleMessageGetFile(from string, msg MessageGetFile) error
 		return err
 	}
 
+	rc, ok := r.(io.ReadCloser)
+	if ok {
+		fmt.Println("closing ReadCloser")
+		rc.Close()
+	}
+
 	peer, ok := s.peers[from]
 	if !ok {
 		return fmt.Errorf("peer %s not in map", from)
