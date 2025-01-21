91eb8e0562b3939770eaa918863f4ba87e3ac4de
daniilkuz <d.d.kuzovlev@gmail.com>
2025-01-21 22:31:38 +0300

    small fixes

--- crypto_test.go
+++ crypto_test.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
 
 func TestCopyEnrypt(t *testing.T) {
 	src := bytes.NewReader([]byte("Good moring!"))
 	dst := new(bytes.Buffer)
+	key := newEncryptionKey()
+	_, err := coppyEncrypt(key, src, dst)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(dst.Bytes())
 }
