af5d67b1e908f45cc3fe7da5f7d051758c2e698c
daniilkuz <d.d.kuzovlev@gmail.com>
2025-01-25 22:01:13 +0300

    added logic to copyDecrypt

--- crypto.go
+++ crypto.go
@@ -23,6 +23,29 @@ func copyDecrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
 	if _, err := src.Read(iv); err != nil {
 		return 0, err
 	}
+
+	var (
+		buf    = make([]byte, 32*1024)
+		stream = cipher.NewCTR(block, iv)
+	)
+
+	for {
+		n, err := src.Read(buf)
+		if n > 0 {
+			stream.XORKeyStream(buf, buf[:n])
+			if _, err := dst.Write(buf[:n]); err != nil {
+				return 0, err
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return 0, err
+		}
+	}
+	return 0, nil
 }
 
 func copyEncrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
