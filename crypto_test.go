e7361d0c60a2ba1a252a1c18f423ea69791a97d6
daniilkuz <d.d.kuzovlev@gmail.com>
2025-01-27 23:43:51 +0300

    added copyDecrypt

--- crypto_test.go
+++ crypto_test.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 )
 
 func TestCopyEncryptDecrypt(t *testing.T) {
-	src := bytes.NewReader([]byte("Good moring!"))
+	payload := "Good moring!"
+	src := bytes.NewReader([]byte(payload))
 	dst := new(bytes.Buffer)
 	key := newEncryptionKey()
 	_, err := copyEncrypt(key, src, dst)
 	if err != nil {
 		t.Error(err)
 	}
-	fmt.Println(dst.Bytes())
+
+	// fmt.Println(dst.String())
+
+	out := new(bytes.Buffer)
+	if _, err := copyDecrypt(key, dst, out); err != nil {
+		t.Error(err)
+	}
+
+	if out.String() != payload {
+		t.Error("Decryption failed!")
+	}
+
+	// fmt.Println(out.String())
 }
